a79ea8fc1a2ee29d7559d2a554f2e602a29efd29
HoppingGanon <hoppingganonapp@gmail.com>
2023-04-15 10:51:58 +0900

    [Add] 処理の一部を分離

--- main.py
+++ main.py
@@ -6,51 +6,75 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 
-# フォルダを開くダイアログを表示して、選択されたフォルダをrootDirに代入する
-root = tk.Tk()
-root.withdraw()
-rootDir = filedialog.askdirectory()
+def get_text_wav(filePath, model):
+    # 音声ファイルの読み込み
+    print(filePath)
+    wf = wave.open(filePath, "rb")
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print ("Audio file must be WAV format mono PCM.")
+        exit (1)
 
-# スクリプトと同じディレクトリにあるモデルを読み込む
-model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")
-if not os.path.exists(model_path):
-    messagebox.showwarning("警告", "モデルフォルダが見つかりません。アプリケーションを終了します。")
-    exit(1)
-model = vosk.Model(model_path)
+    # VOSKでの音声認識
+    rec = vosk.KaldiRecognizer(model, wf.getframerate())
+    rec.SetWords(True)
 
-# 認識結果を格納する辞書を初期化
-final_results = {}
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        rec.AcceptWaveform(data)
 
-# 再帰的に検索
-for dirName, subdirList, fileList in os.walk(rootDir):
-    for filename in fileList:
-        # WAVファイルであれば処理を実行
-        if filename.lower().endswith(".wav"):
-            filePath = os.path.join(dirName, filename)
+    # 認識結果をfinal_resultsに追加
+    result = json.loads(rec.FinalResult())
+    return result
 
-            # 音声ファイルの読み込み
-            print(filePath)
-            wf = wave.open(filePath, "rb")
-            if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-                print ("Audio file must be WAV format mono PCM.")
-                exit (1)
+class TextResult():
+    path: str
+    obj: any
 
-            # VOSKでの音声認識
-            rec = vosk.KaldiRecognizer(model, wf.getframerate())
-            rec.SetWords(True)
+    def __init__(self) -> None:
+        self.path = ""
+        self.obj = {}
+        pass
 
-            while True:
-                data = wf.readframes(4000)
-                if len(data) == 0:
-                    break
-                rec.AcceptWaveform(data)
+def get_text(dirName, filename, model) -> TextResult:
+    # WAVファイルであれば処理を実行
+    r = TextResult()
 
-            # 認識結果をfinal_resultsに追加
-            result = json.loads(rec.FinalResult())["text"]
-            final_results[filePath] = result
-            print(result)
+    if filename.lower().endswith(".wav"):
+        filePath = os.path.join(dirName, filename).replace("\\", "/")
+        r.path = filePath
+        r.obj = get_text_wav(filePath, model)
 
-# final_resultsをJSONファイルとして出力
-outPath = os.path.join(rootDir, "results.json")
-with open(outPath, "w") as f:
-    f.write(json.dumps(final_results, ensure_ascii=False))
+    return r
+
+def main():
+    # フォルダを開くダイアログを表示して、選択されたフォルダをrootDirに代入する
+    root = tk.Tk()
+    root.withdraw()
+    rootDir = filedialog.askdirectory()
+
+    # スクリプトと同じディレクトリにあるモデルを読み込む
+    model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")
+    if not os.path.exists(model_path):
+        messagebox.showwarning("警告", "モデルフォルダが見つかりません。アプリケーションを終了します。")
+        exit(1)
+    model = vosk.Model(model_path)
+
+    # 認識結果を格納する辞書を初期化
+    final_results = {}
+
+    # 再帰的に検索
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        for filename in fileList:
+            r = get_text(dirName, filename, model)
+            if r.path != "":
+                final_results[r.path] = r.obj
+
+    # final_resultsをJSONファイルとして出力
+    outPath = os.path.join(rootDir, "results.json")
+    with open(outPath, "w", encoding="utf-8") as f:
+        f.write(json.dumps(final_results, ensure_ascii=False))
+        f.close()
+
+main()
