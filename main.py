f729b144096ea5e4877613879ebd9715cb4f9983
HoppingGanon <hoppingganonapp@gmail.com>
2023-04-15 11:33:35 +0900

    [Add] mp3を処理するひな型を追加

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import vosk
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+from ffmpeg import input, output
+
 
 def get_text_wav(filePath, model):
     # 音声ファイルの読み込み
@@ -28,6 +30,18 @@ def get_text_wav(filePath, model):
     result = json.loads(rec.FinalResult())
     return result
 
+
+def convert_mp3_to_wav(fullname, name, output_folder_path):
+    wav_file_path = os.path.join(output_folder_path, f"{name}.wav")
+
+    input_file = input(fullname)
+    audio_stream = input_file.audio
+    audio_stream = audio_stream.filter('aformat', 's16:44100').output(wav_file_path)
+    output(audio_stream, quiet=True).overwrite_output().run()
+
+    return wav_file_path
+
+
 class TextResult():
     path: str
     obj: any
@@ -37,22 +51,37 @@ class TextResult():
         self.obj = {}
         pass
 
+
 def get_text(dirName, filename, model) -> TextResult:
-    # WAVファイルであれば処理を実行
+    # WAVファイルまたはMP3ファイルであれば処理を実行
     r = TextResult()
 
     if filename.lower().endswith(".wav"):
         filePath = os.path.join(dirName, filename).replace("\\", "/")
         r.path = filePath
         r.obj = get_text_wav(filePath, model)
+    elif filename.lower().endswith(".mp3"):
+        mp3_file_path = os.path.join(dirName, filename).replace("\\", "/")
+        output_folder_path = os.path.join(os.environ["LOCALAPPDATA"], "HoppingGanon", "audio2text", "cache").replace("\\", "/")
+
+        if not os.path.exists(output_folder_path):
+            os.makedirs(output_folder_path)
+
+        wav_file_path = convert_mp3_to_wav(mp3_file_path, filename, output_folder_path)
+        r.path = mp3_file_path
+        r.obj = get_text_wav(wav_file_path, model)
 
     return r
+
 
 def main():
     # フォルダを開くダイアログを表示して、選択されたフォルダをrootDirに代入する
     root = tk.Tk()
     root.withdraw()
     rootDir = filedialog.askdirectory()
+
+    if rootDir == "":
+        exit(0)
 
     # スクリプトと同じディレクトリにあるモデルを読み込む
     model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")
@@ -77,4 +106,6 @@ def main():
         f.write(json.dumps(final_results, ensure_ascii=False))
         f.close()
 
-main()
+
+if __name__ == '__main__':
+    main()
